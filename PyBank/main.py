ea6a6a2ad35ea1aa501dcececcb26f18f134d1af
Colleen-Cobb <colleen.cobb5@gmail.com>
2022-10-01 13:01:01 -0400

    pyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,6 +15,10 @@ outpath = os.path.join("analysis", "results.txt")
 
 total_months = 0
 total_net= 0
+month= []
+profits_losses= []
+last_value= 0
+changes= 0
 
 
 
@@ -27,6 +31,15 @@ with open(budgetpath) as csv_file:
     csv_first = next (csv_reader)
     
     total_months = total_months + 1
+
+    
+
+   
+  
+
+
+
+   
     #one line of code for the total net and one line for the previous net 
     total_net += int(csv_first[1])
     previous_net= int(csv_first[1])
@@ -36,21 +49,41 @@ with open(budgetpath) as csv_file:
         total_months+=1
         total_net += int(row[1])
 
+        #Keep track of the month
+        month.append(row[0])
+
+        #Keep track of the changes in profi/losses
+        changes = int(row[1])-last_value
+        profits_losses.append(changes)
+        last_value= int(csv_first[1])
+
+
 
         #net change
 
-        previous_net = (int(row[1])-previous_net)/
+        
+        
 
-        #if statement to caluclate the greatest increase
+        #caluclate the greatest increase
+        greatest_increase= max(profits_losses)
+
+        greatest_inc_value= profits_losses.index(greatest_increase)
+
+        greatest_inc_month= month[greatest_inc_value]
 
 
-        #if statement to calculate the greatest decrease
+        #calculate the greatest decrease
+  
+        greatest_decrease= min (profits_losses)
 
+        greatest_dec_value= profits_losses.index(greatest_decrease)
+
+        greatest_dec_month= month[greatest_dec_value]
 
 
 #net monthly average 
 
-
+average_change= sum(profits_losses)/len(profits_losses)
 
 
 
@@ -65,7 +98,9 @@ output= (
     f"------------------\n"
     f"Total Months: {total_months}\n"
     f"Total: ${total_net}\n"
-    f"Average Change: ${previous_net}\n"
+    f"Average Change: ${str(round(average_change,2))}\n"
+    f"Greatest Increase in Profits: {greatest_inc_month} (${str(greatest_increase)})\n"
+    f"Greastest Decrease in Profits: {greatest_dec_month} (${str(greatest_decrease)})\n"
 
 
 
