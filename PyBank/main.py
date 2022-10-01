e455737d94abcbe8761ed6de403e7b18fede57b8
Colleen-Cobb <colleen.cobb5@gmail.com>
2022-09-30 20:23:15 -0400

    python project

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,33 +1,83 @@
+#Libraries 
 import os
 
 import csv
 
-print(dir(csv))
-
 print(os.getcwd())
 
+#Input File
 
-budgetcsv = os.path.join("Resources", "budget_data.csv")
+#Output File
 
-print("Financial Analysis")
+budgetpath = os.path.join("Resources", "budget_data.csv")
+outpath = os.path.join("analysis", "results.txt")
+#Variables
 
-with open(budgetcsv) as csv_file: 
+total_months = 0
+total_net= 0
+
+
+
+
+with open(budgetpath) as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter = ",")
 
-    csv_header = next(csv_file)
-    # print(f"Header: {csv_header}")
+    csv_header = next(csv_reader)
 
-    # Print the tota number of months included in the dataset
+    csv_first = next (csv_reader)
+    
+    total_months = total_months + 1
+    #one line of code for the total net and one line for the previous net 
+    total_net += int(csv_first[1])
+    previous_net= int(csv_first[1])
 
-    for row in cvsreader:
-        print("Total Months: ",len(row))
-#     list= (row[0])
-#     def Number_of_Months(row[0]):
-#         count=0
-#         for months in row[0]:
-#             count+= 1
-#         return count
-# print("Total Months: ", Number_of_Months(row[0]))
+    for row in csv_reader:
+
+        total_months+=1
+        total_net += int(row[1])
+
+
+        #net change
+
+        previous_net = (int(row[1])-previous_net)/
+
+        #if statement to caluclate the greatest increase
+
+
+        #if statement to calculate the greatest decrease
+
+
+
+#net monthly average 
+
+
+
+
+
+
+
+
+
+
+#format output
+output= (
+    f"Financial Analysis\n"
+    f"------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: ${previous_net}\n"
+
+
+
+
+)
+
+print(output)
+
+with open(outpath,"w") as text_file: 
+    text_file.write(output)
+       
+#
 
 
     #The net total amount of 'Profit/Losses' over the entire perioed, and then the average of those changes
